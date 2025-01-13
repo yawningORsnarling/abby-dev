2e7ff3b463d34606847a64a481500d59768fc80d
Shaun Phillips <sphillips@coreweave.com>
2025-01-13 13:37:25 -0500

    fix: attempting lazyload again - 2

--- js/flickity.js
+++ js/flickity.js
@@ -2,7 +2,7 @@
 /* Theatre */
 var tc1 = document.querySelector('.theatre-carousel-1');
 var flkty = new Flickity( tc1, {
-    // lazyLoad: true,
+    lazyLoad: true,
     imagesLoaded: true,
     contain: true,
     autoPlay: true,
