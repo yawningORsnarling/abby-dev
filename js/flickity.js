3fd42a897bca93a8d9ab0dc5584970e4c15dcbe0
Shaun Phillips <sphillips@coreweave.com>
2025-01-19 18:41:14 -0500

    fix: testing ads scrolling - 3

--- js/flickity.js
+++ js/flickity.js
@@ -1,7 +1,7 @@
 
 /* Theatre */
 var tc1 = document.querySelector('.theatre-carousel-1');
-var flkty = new Flickity( tc1, {
+var flkty_tc1 = new Flickity( tc1, {
     lazyLoad: true,
     imagesLoaded: true,
     contain: true,
@@ -13,7 +13,7 @@ var flkty = new Flickity( tc1, {
 
 /* Commercial */
 var cc1 = document.querySelector('.commercial-carousel-1');
-var flkty = new Flickity( cc1, {
+var flkty_cc1 = new Flickity( cc1, {
     selectedAttraction: 0.02,
     friction: 1.5,
     autoPlay: true,
@@ -24,17 +24,22 @@ var flkty = new Flickity( cc1, {
     prevNextButtons: false,
     pageDots: false
 });
-flkty.on('staticClick', function() {
-    flkty.playPlayer();
-});
+// Override the stopPlayer method
+flkty_cc1.stopPlayer = function() {
+    this.player.stop();
+    // Resume autoPlay after 3 seconds
+    setTimeout(() => {
+        this.player.play();
+    }, 1000);
+};
 var cc2 = document.querySelector('.commercial-carousel-2');
-var flkty = new Flickity( cc2, {
+var flkty_cc2 = new Flickity( cc2, {
     autoPlay: false,
     wrapAround: true,
     pageDots: false
 });
 var cc3 = document.querySelector('.commercial-carousel-3');
-var flkty = new Flickity( cc3, {
+var flkty_cc3 = new Flickity( cc3, {
     selectedAttraction: 0.02,
     friction: 1.5,
     autoPlay: true,
@@ -46,13 +51,13 @@ var flkty = new Flickity( cc3, {
 
 /* Resume */
 var rc1 = document.querySelector('.resume-carousel-1');
-var flkty = new Flickity( rc1, {
+var flkty_rc1 = new Flickity( rc1, {
     lazyLoad: true,
     wrapAround: false,
     pageDots: false,
 });
 var rc2 = document.querySelector('.resume-carousel-2');
-var flkty = new Flickity( rc2, {
+var flkty_rc2 = new Flickity( rc2, {
     lazyLoad: true,
     wrapAround: false,
     pageDots: false,
